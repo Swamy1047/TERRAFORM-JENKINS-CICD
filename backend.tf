096da16fe72b1ea8753dc03d19a17aa64ca0484f
Swamy1047 <140911560+Swamy1047@users.noreply.github.com>
2023-10-03 15:04:43 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "ajay-mrcloudbook777"
+    bucket         = "swamy1047-tfstate-save"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    dynamodb_table = "swamy-dynamo-db-table"
   }
 }
