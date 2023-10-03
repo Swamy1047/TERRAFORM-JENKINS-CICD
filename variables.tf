baac236e9ad4c8996a861fde75c46dcd0b570de2
Swamy1047 <140911560+Swamy1047@users.noreply.github.com>
2023-10-03 15:12:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "ap-south-1"
+  default     = "ap-southeast-1"
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "Mumbai"
+  default     = "mns"
 }
 variable "instance_type" {
   description = "instance type for ec2"
@@ -12,10 +12,10 @@ variable "instance_type" {
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0f5ee92e2d63afc18"
+  default     = "ami-0df7a207adb9748c7"
 }
 variable "bucketname" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "ajaykumar-yegireddi-cloud"
+  default     = "swamys3-bucket-project-1047"
 }
